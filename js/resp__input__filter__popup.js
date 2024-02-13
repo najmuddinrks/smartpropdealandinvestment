49448c2570b797ad83d466100e758a0de507371c
najmuddin ahmed ali shaikh <130458150+najmuddinrks@users.noreply.github.com>
2024-02-13 19:11:10 +0530

    js file adjustment

--- js/resp__input__filter__popup.js
+++ js/resp__input__filter__popup.js
@@ -37,91 +37,79 @@ $(document).ready(function () {
         accordionContent.slideToggle();
         accordionContent.siblings('.accordion-content3').slideUp();
 
-
         var downArrow = $(this).find('.fa-chevron-down');
         var upArrow = $(this).find('.fa-chevron-up');
         downArrow.toggleClass('d-none', accordionItem.hasClass('active'));
         upArrow.toggleClass('d-none', !accordionItem.hasClass('active'));
     });
 
-    document.querySelector('.bundle__chuck').addEventListener('click', function() {
-        this.classList.add('d-none');
-        document.querySelector('.hidden__content').classList.toggle('hidden');
-    });
+    // document.querySelector('.bundle__chuck').addEventListener('click', function() {
+    //     this.classList.add('d-none');
+    //     document.querySelector('.hidden__content').classList.toggle();
+    // });
     
     
-    $('.bundle__chuck2').click(function () {
+    $('.bundle__chuck').click(function () {
         $(this).addClass("d-none");
-        $('.hidden__content2').toggle(); 
+        $(this).next('.hidden__content').toggle(); 
     });
+    
 
     $('#toggleButton').change(function () {
         var toggleStatus = $(this).prop('checked');
         $('#toggleStatus').text(toggleStatus ? "true" : "false");
     });
-
-    $('.form-check-input').on('change', function () {
-        if ($(this).is(':checked')) {
-          console.log('Checkbox is checked.');
-        } else {
-          console.log('Checkbox is unchecked.');
-        }
-      });
-
-      
-         // Initialize slider
-    $("#slider143").slider({
-        range: true,
-        min: 0,
-        max: 500,
-        values: [75, 300],
-        slide: function(event, ui) {
-            $(".from").val(ui.values[0]);
-            $(".to").val(ui.values[1]);
-        }
-    });
-
-    // From input change event
-    $(".from").change(function() {
-        var value = parseInt($(this).val());
-        if (isNaN(value)) {
-            value = 0;
-        }
-        $("#slider143").slider("values", 0, value);
-    });
-
-    // To input change event
-    $(".to").change(function() {
-        var value = parseInt($(this).val());
-        if (isNaN(value)) {
-            value = 0;
-        }
-        $("#slider143").slider("values", 1, value);
-    });
-
-    // Dropdown change event for MinSelect
-    $("#minSelect").change(function() {
-        var selectedValue = $(this).val();
-        // Get the numeric value from the selected option
-        var numericValue = parseFloat(selectedValue.match(/(\d+)/)[0]);
-        if (!isNaN(numericValue)) {
-            $("#slider143").slider("values", 0, numericValue);
-        }
-    });
-
-    // Dropdown change event for MaxSelect
-    $("#maxSelect").change(function() {
-        var selectedValue = $(this).val();
-        // Get the numeric value from the selected option
-        var numericValue = parseFloat(selectedValue.match(/(\d+)/)[0]);
-        if (!isNaN(numericValue)) {
-            $("#slider143").slider("values", 1, numericValue);
-        }
-    });
-
 });
+    // $('.form-check-input').on('change', function () {
+    //     if ($(this).is(':checked')) {
+    //       console.log('Checkbox is checked.');
+    //     } else {
+    //       console.log('Checkbox is unchecked.');
+    //     }
+    //   });
+
+// $("#slider143").slider({
+//     range: true,
+//     min: 0,
+//     max: 500,
+//     values: [75, 300],
+//     slide: function(event, ui) {
+//         $(".from").val(ui.values[0]);
+//         $(".to").val(ui.values[1]);
+//     }
+// });
 
 
+// $(".from").change(function() {
+//     var value = parseInt($(this).val());
+//     if (isNaN(value)) {
+//         value = 0;
+//     }
+//     $("#slider143").slider("values", 0, value);
+// });
+
+
+// $(".to").change(function() {
+//     var value = parseInt($(this).val());
+//     if (isNaN(value)) {
+//         value = 0;
+//     }
+//     $("#slider143").slider("values", 1, value);
+// });
+// $("#minSelect").change(function() {
+//     var selectedValue = $(this).val();
+//     var numericValue = parseFloat(selectedValue.match(/(\d+)/)[0]);
+//     if (!isNaN(numericValue)) {
+//         $("#slider143").slider("values", 0, numericValue);
+//     }
+// });
+// $("#maxSelect").change(function() {
+//     var selectedValue = $(this).val();
+//     var numericValue = parseFloat(selectedValue.match(/(\d+)/)[0]);
+//     if (!isNaN(numericValue)) {
+//         $("#slider143").slider("values", 1, numericValue);
+//     }
+// });
 
 
 // var select = document.getElementById('input-select');
@@ -190,41 +178,36 @@ $(document).ready(function () {
 // nonLinearSlider.noUiSlider.on('update', function (values, handle, unencoded, isTap, positions) {
 //     nodes[handle].innerHTML = values[handle] + ', ' + positions[handle].toFixed(2) + '%';
 // });
-$(document).ready(function() {
-    // Populate the select options
-    var step = 50000; // Increment value
-    var minVal = 50000; // Minimum value
-    var maxVal = 200000000; // Maximum value
+// $(document).ready(function() {
+//     var step = 50000;
+//     var minVal = 50000; 
+//     var maxVal = 200000000;
 
-    for (var i = minVal; i <= maxVal; i += step) {
-        $(".from").append("<option value='" + i + "'>" + i + "</option>");
-        $(".to").append("<option value='" + i + "'>" + i + "</option>");
-    }
+//     for (var i = minVal; i <= maxVal; i += step) {
+//         $(".from").append("<option value='" + i + "'>" + i + "</option>");
+//         $(".to").append("<option value='" + i + "'>" + i + "</option>");
+//     }
 
-    // Update slider when 'from' select changes
-    $(".from").change(function() {
-        var value = $(this).val();
-        $("#slider143").slider("values", 0, value);
-    });
+//     $(".from").change(function() {
+//         var value = $(this).val();
+//         $("#slider143").slider("values", 0, value);
+//     });
+//     $(".to").change(function() {
+//         var value = $(this).val();
+//         $("#slider143").slider("values", 1, value);
+//     });
 
-    // Update slider when 'to' select changes
-    $(".to").change(function() {
-        var value = $(this).val();
-        $("#slider143").slider("values", 1, value);
-    });
-
-    // Update select elements when slider changes
-    $("#slider143").slider({
-        range: true,
-        min: minVal,
-        max: maxVal,
-        values: [7500000, 30000000],
-        slide: function (event, ui) {
-            $(".from").val(ui.values[0]);
-            $(".to").val(ui.values[1]);
-        }
-    });
-});
+//     $("#slider143").slider({
+//         range: true,
+//         min: minVal,
+//         max: maxVal,
+//         values: [7500000, 30000000],
+//         slide: function (event, ui) {
+//             $(".from").val(ui.values[0]);
+//             $(".to").val(ui.values[1]);
+//         }
+//     });
+// });
 
 
 // $(document).ready(function() {
@@ -243,3 +226,85 @@ $(document).ready(function() {
 //         $("#slider143").slider("value", selectedValue);
 //     });
 // });
+$(function() {
+    $(".slider143").slider({
+      range: true,
+      min: 0,
+      max: 300, // Adjusted max value to 300
+      values: [0, 300], // Adjusted initial values
+      slide: function(event, ui) {
+        $(".from").val(ui.values[0]);
+        $(".to").val(ui.values[1]);
+      }
+    });
+  
+    $(".from").change(function() {
+      var value = parseInt($(this).val());
+      var toValue = parseInt($(".to").val());
+      if (value > toValue) {
+        value = toValue;
+      }
+      $(".slider143").slider("values", 0, value);
+    });
+  
+    $(".to").change(function() {
+      var value = parseInt($(this).val());
+      var fromValue = parseInt($(".from").val());
+      if (value < fromValue) {
+        value = fromValue;
+      }
+      $(".slider143").slider("values", 1, value);
+    });
+  
+    $(".from, .to").on("change", function() {
+      var fromValue = parseInt($(".from").val());
+      var toValue = parseInt($(".to").val());
+      $(".slider143").slider("values", [fromValue, toValue]);
+    });
+  
+    $(".slider143").on("slide", function(event, ui) {
+      $(".from").val(ui.values[0]);
+      $(".to").val(ui.values[1]);
+    });
+  });
+  $(function() {
+    $(".slider__length__direction").slider({
+      range: true,
+      min: 0,
+      max: 300, // Adjusted max value to 300
+      values: [0, 300], // Adjusted initial values
+      slide: function(event, ui) {
+        $(".from").val(ui.values[0]);
+        $(".to").val(ui.values[1]);
+      }
+    });
+  
+    $(".from").change(function() {
+      var value = parseInt($(this).val());
+      var toValue = parseInt($(".to").val());
+      if (value > toValue) {
+        value = toValue;
+      }
+      $(".slider__length__direction").slider("values", 0, value);
+    });
+  
+    $(".to").change(function() {
+      var value = parseInt($(this).val());
+      var fromValue = parseInt($(".from").val());
+      if (value < fromValue) {
+        value = fromValue;
+      }
+      $(".slider__length__direction").slider("values", 1, value);
+    });
+  
+    $(".from, .to").on("change", function() {
+      var fromValue = parseInt($(".from").val());
+      var toValue = parseInt($(".to").val());
+      $(".slider__length__direction").slider("values", [fromValue, toValue]);
+    });
+  
+    $(".slider__length__direction").on("slide", function(event, ui) {
+      $(".from").val(ui.values[0]);
+      $(".to").val(ui.values[1]);
+    });
+  });
